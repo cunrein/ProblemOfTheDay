b990ee6a20474c04bfe50b8324385f1f9ab8f59f
Craig Unrein <cunrein@amazon.com>
2018-12-27 13:42:23 -0800

    Solution to advent day 9 and problem 31.

--- src/problemSeven/decodePerms.kt
+++ src/problemSeven/decodePerms.kt
@@ -7,12 +7,12 @@ fun countNumberDecodings(message: CharArray, len: Int): Int {
     if (len == 0 || len == 1) return 1
     var count = 0
     if (message[len - 1] > '0') {
-        count = countNumberDecodings(message, len - 1)
+        count = problemSeven.countNumberDecodings(message, len - 1)
     }
     if (message[len - 2] == '1' ||
         (message[len - 2] == '2' && message[len - 1] < '7')
     ) {
-        count += countNumberDecodings(message, len - 2)
+        count += problemSeven.countNumberDecodings(message, len - 2)
     }
 
     return count
